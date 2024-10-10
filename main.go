4b04d2794293995ef233100e8d863efff6b78317
almaz <tyj5resd@gmail.com>
2024-10-10 05:38:06 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func pong(c *gin.Context) {
+	c.JSON(http.StatusOK, "pong")
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", pong)
+
+	err := router.Run(":8080")
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+	}
+}
